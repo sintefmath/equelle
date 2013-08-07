5228f2be913c20549133461ce45f8e9869d597a3
Jens Olav Nygaard <Jens.O.Nygaard@sintef.no>
2013-08-07 13:51:01 +0200

    - Minor cleanup of the test case. - Added necessary minimum of functionality to the example class   CollOfScalarsOnColl and EquelleRuntimeCPU::output. - Switched the boundaryCell() implementation to regard a 2D(n, 1)   grid as a true 2D and not 1D grid, i.e., all cells of such a grid   become boundary cells. This for the sake of the example.

--- examples/1D_heat_implicit/app/OnTest1.cpp
+++ examples/1D_heat_implicit/app/OnTest1.cpp
@@ -27,29 +27,27 @@ int main(int argc, char** argv)
     // Create the Equelle runtime.
     EquelleRuntimeCPU er(param);
 
-    // all_cells : Collection Of Cells On AllCells() = AllCells()
+    // EQUELLE: all_cells : Collection Of Cells On AllCells() = AllCells()
     CollOfCells all_cells = er.allCells();
 
-    std::cout << "Total number of cells: " << all_cells.size() << std::endl;
-
-    // boundary_cells : Collection Of Cells On BoundaryCells() = BoundaryCells()
+    // EQUELLE: boundary_cells : Collection Of Cells On BoundaryCells() = BoundaryCells()
     CollOfCells boundary_cells = er.boundaryCells();
 
-    std::cout << "Number of boundary cells: " << boundary_cells.size() << std::endl;
-
-    // all_vol : Collection Of Scalar On all_cells = Volume(all_cells)
+    // EQUELLE: all_vol : Collection Of Scalar On all_cells = Volume(all_cells)
     CollOfScalars all_vol = er.volume( all_cells );
-    CollOfScalarsOnColl all_vol_2( er.volume(all_cells), all_cells );
+    CollOfScalarsOnColl all_vol_2 = CollOfScalarsOnColl( er.volume(all_cells), all_cells );
 
-    // boundary_vol : Collection Of Scalar On boundary_cells = Volume(boundary_cells)
+    // If something like this is the way to go, EquelleRuntimeCPU::volume() etc. should be modified instead of this way of using the constructor
+
+    // EQUELLE: boundary_vol : Collection Of Scalar On boundary_cells = Volume(boundary_cells)
     CollOfScalars boundary_vol = er.volume( boundary_cells );
-    CollOfScalarsOnColl boundary_vol_2( er.volume(boundary_cells), boundary_cells );
+    CollOfScalarsOnColl boundary_vol_2 = CollOfScalarsOnColl( er.volume(boundary_cells), boundary_cells );
 
-    // vol_diff : Collection Of Scalar On AllCells() = all_vol - boundary_vol # NB! Should fail, preferably in the Equelle compiler
+    // EQUELLE: vol_diff : Collection Of Scalar On AllCells() = all_vol - boundary_vol # NB! Should fail, preferably in the Equelle compiler
     CollOfScalars vol_diff = all_vol - boundary_vol;
     CollOfScalarsOnColl vol_diff_2 = all_vol_2 - boundary_vol_2;
 
-    // Output(vol_diff)
+    // EQUELLE: Output(vol_diff)
     er.output("vol_diff:       ",       vol_diff);
     er.output("all_vol_2:      ",      all_vol_2);
     er.output("boundary_vol_2: ", boundary_vol_2);
--- examples/1D_heat_implicit/src/EquelleRuntimeCPU.cpp
+++ examples/1D_heat_implicit/src/EquelleRuntimeCPU.cpp
@@ -38,7 +38,8 @@ bool EquelleRuntimeCPU::boundaryCell(const int cell_index) const
 {
     const int fp = grid_.cell_facepos[cell_index];
     bool boundary_cell = false;
-#if 0
+    const int f_per_c = grid_.dimensions*2; // Can we assume this, or should we use cell_facepos[c+1]-cell_facepos[c]?!
+#if 1
     for (int i=0; (i<f_per_c) && (!boundary_cell); ++i) {
         const int f = grid_.cell_faces[ fp+i ];
         boundary_cell = (grid_.face_cells[2*f]==-1) || (grid_.face_cells[2*f + 1]==-1);
@@ -46,7 +47,6 @@ bool EquelleRuntimeCPU::boundaryCell(const int cell_index) const
 #else
     // Hack for 1D grid realized as 2D (n, 1) or (1, n)
     int num_of_boundary_faces = 0;
-    const int f_per_c = grid_.dimensions*2; // Can we assume this, or should we use cell_facepos[c+1]-cell_facepos[c]?!
     for (int i=0; i<f_per_c; ++i) {
         const int f = grid_.cell_faces[ fp+i ];
         if ( (grid_.face_cells[2*f]==-1) || (grid_.face_cells[2*f + 1]==-1) ) {
@@ -319,6 +319,13 @@ void EquelleRuntimeCPU::output(const std::string& tag, const CollOfScalars& vals
         std::cout << std::setw(15) << std::left << ( vals[i] ) << " ";
     }
     std::cout << std::endl;
+}
+
+
+void EquelleRuntimeCPU::output(const std::string& tag, const CollOfScalarsOnColl& vals)
+{
+    output(tag, vals.getColl());
+    std::cout << "(This was On Collection " << vals.getOnColl() << ")" << std::endl;
 }
 
 
