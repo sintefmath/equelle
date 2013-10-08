61eba61bd9a0364eaf1c713be01d8b79dada92a2
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2013-10-08 22:13:48 +0200

    Add lots of AST nodes and parse actions.

--- atgeirrcompiler/Parser.hpp
+++ atgeirrcompiler/Parser.hpp
@@ -10,6 +10,8 @@
 #include <sstream>
 #include <map>
 #include <vector>
+#include <algorithm>
+#include <stdexcept>
 
 
 // ------ Declarations needed for bison parser ------ 
@@ -63,6 +65,24 @@ inline std::string basicTypeString(const BasicType bt)
     }
 }
 
+inline bool isEntityType(const BasicType bt)
+{
+    switch (bt) {
+    case Bool:
+    case Scalar:
+    case Vector:
+        return false;
+    case Cell:
+    case Face:
+    case Edge:
+    case Vertex:
+        return true;
+    default:
+        yyerror("internal compiler error in isEntityType().");
+        return false;
+    }
+}
+
 enum CanonicalEntitySet { InteriorCells = 0, BoundaryCells, AllCells,
                           InteriorFaces, BoundaryFaces, AllFaces,
                           InteriorEdges, BoundaryEdges, AllEdges,
@@ -104,6 +124,33 @@ public:
     {
     }
 
+    bool isBasic() const
+    {
+        return (basic_type_ != Invalid)
+            && (collection_ == false)
+            && (gridmapping_ == NotApplicable);
+    }
+
+    bool isEntityCollection() const
+    {
+        return isEntityType(basic_type_) && collection_;
+    }
+
+    BasicType basicType() const
+    {
+        return basic_type_;
+    }
+
+    bool isCollection() const
+    {
+        return collection_;
+    }
+
+    int gridMapping() const
+    {
+        return gridmapping_;
+    }
+
     std::string equelleString() const
     {
         std::string es = collection_ ? "Collection Of " : "";
@@ -138,50 +185,197 @@ private:
 
 // ------ SymbolTable singleton ------ 
 
+
+
 class EntitySet
 {
+};
+
+
+class FunctionType
+{
+public:
+    typedef std::pair<std::string, EquelleType> ArgumentType;
+
+    FunctionType(const EquelleType return_type)
+        : return_type_(return_type)
+    {
+    }
+
+    FunctionType(const EquelleType return_type,
+                 const std::string arg1_name, const EquelleType arg1_type)
+        : return_type_(return_type),
+          arguments_(1, std::make_pair(arg1_name, arg1_type))
+    {
+    }
+
+    FunctionType(const EquelleType return_type,
+                 const std::vector<ArgumentType>& args)
+        : return_type_(return_type),
+          arguments_(args)
+    {
+    }
+
+    EquelleType returnType() const
+    {
+        return return_type_;
+    }
+
+    const std::vector<ArgumentType> arguments() const
+    {
+        return arguments_;
+    }
+
+private:
+    EquelleType return_type_;
+    std::vector<ArgumentType> arguments_;
 };
 
 
 class Function
 {
 public:
+    Function(const std::string& name, const FunctionType& type)
+        : name_(name),
+          type_(type)
+    {
+    }
+
     void declareVariable(const std::string name, const EquelleType type)
     {
-        if (variable_types_.find(name) == variable_types_.end()) {
-            variable_types_[name] = type;
+        if (!declared(name).first) {
+            local_variable_types_[name] = type;
         } else {
             std::string errmsg = "redeclared variable: ";
             errmsg += name;
             yyerror(errmsg.c_str());
         }
     }
+
+    EquelleType variableType(const std::string name) const
+    {
+        auto foundvar = declared(name);
+        if (!foundvar.first) {
+            std::string err_msg = "could not find variable ";
+            err_msg += name;
+            yyerror(err_msg.c_str());
+            return EquelleType();
+        } else {
+            return foundvar.second;
+        }
+    }
+
+    std::string name() const
+    {
+        return name_;
+    }
+    EquelleType returnType() const
+    {
+        return type_.returnType();
+    }
 private:
+    std::pair<bool, EquelleType> declared(const std::string& name) const
+    {
+        auto lit = local_variable_types_.find(name);
+        if (lit != local_variable_types_.end()) {
+            return std::make_pair(true, lit->second);
+        }
+        auto ait = std::find_if(type_.arguments().begin(), type_.arguments().end(),
+                                [&](const FunctionType::ArgumentType& a) { return a.first == name; });
+        if (ait != type_.arguments().end()) {
+            return std::make_pair(true, ait->second);
+        }
+        return std::make_pair(false, EquelleType());
+    }
     std::string name_;
-    std::map<std::string, EquelleType> variable_types_;
-    std::vector<EntitySet> entitysets_;
+    std::map<std::string, EquelleType> local_variable_types_;
+    std::vector<EntitySet> local_entitysets_;
+    FunctionType type_;
 };
 
 
 class SymbolTable
 {
 public:
-    static void declareVariable(const std::string name, const EquelleType type)
+    static void declareVariable(const std::string& name, const EquelleType& type)
     {
         instance().current_function_->declareVariable(name, type);
     }
 
+    static void declareFunction(const std::string& name, const FunctionType& ftype)
+    {
+        instance().declareFunctionImpl(name, ftype);
+    }
+
+    static const Function& getFunction(const std::string& name)
+    {
+        return instance().getFunctionImpl(name);
+    }
+
+    static const Function& getCurrentFunction()
+    {
+        return *instance().current_function_;
+    }
+
 private:
     SymbolTable()
-        : functions_(1),
-          current_function_(functions_.begin())
     {
+        functions_.emplace_back("InteriorCells", EquelleType(Cell, true, InteriorCells));
+        functions_.emplace_back("BoundaryCells", EquelleType(Cell, true, BoundaryCells));
+        functions_.emplace_back("AllCells", EquelleType(Cell, true, AllCells));
+        functions_.emplace_back("InteriorFaces", EquelleType(Cell, true, InteriorFaces));
+        functions_.emplace_back("BoundaryFaces", EquelleType(Cell, true, BoundaryFaces));
+        functions_.emplace_back("AllFaces", EquelleType(Cell, true, AllFaces));
+        functions_.emplace_back("InteriorEdges", EquelleType(Cell, true, InteriorEdges));
+        functions_.emplace_back("BoundaryEdges", EquelleType(Cell, true, BoundaryEdges));
+        functions_.emplace_back("AllEdges", EquelleType(Cell, true, AllEdges));
+        functions_.emplace_back("InteriorVertices", EquelleType(Cell, true, InteriorVertices));
+        functions_.emplace_back("BoundaryVertices", EquelleType(Cell, true, BoundaryVertices));
+        functions_.emplace_back("AllVertices", EquelleType(Cell, true, AllVertices));
+        functions_.emplace_back("Main", EquelleType());
+        current_function_ = functions_.end() - 1;
     }
 
     static SymbolTable& instance()
     {
         static SymbolTable s;
         return s;
+    }
+
+    void declareFunctionImpl(const std::string& name, const FunctionType& ftype)
+    {
+        if (current_function_->name() != "Main") {
+            std::string err_msg = "cannot define new function ";
+            err_msg += name;
+            err_msg += " inside function ";
+            err_msg += current_function_->name();
+            yyerror(err_msg.c_str());
+        } else {
+            auto it = findFunction(name);
+            if (it == functions_.end()) {
+                functions_.emplace_back(name, ftype);
+            }
+        }
+    }
+
+    const Function& getFunctionImpl(const std::string& name) const
+    {
+        auto it = findFunction(name);
+        if (it == functions_.end()) {
+            std::string errmsg = "could not find function ";
+            errmsg += name;
+            yyerror(errmsg.c_str());
+            throw std::logic_error("Function not found.");
+        } else {
+            return *it;
+        }
+    }
+
+    std::vector<Function>::const_iterator findFunction(const std::string& name) const
+    {
+        auto it = std::find_if(functions_.begin(), functions_.end(),
+                               [&](const Function& f) { return f.name() == name; });
+        return it;
     }
 
     std::vector<Function> functions_;
@@ -198,35 +392,259 @@ class Node
 public:
     virtual ~Node()
     {}
+    virtual EquelleType type() const
+    {
+        return EquelleType();
+    }
 };
 
 typedef Node* NodePtr;
 
-class Number : public Node
+class NumberNode : public Node
 {
 public:
-    Number(const double num) : num_(num) {}
+    NumberNode(const double num) : num_(num) {}
 private:
     double num_;
 };
 
+class TypeNode : public Node
+{
+public:
+    TypeNode(const EquelleType et) : et_(et) {}
+    EquelleType type() const
+    {
+        return et_;
+    }
+private:
+    EquelleType et_;
+};
 
+typedef TypeNode* TypeNodePtr;
+
+class FuncTypeNode : public Node
+{
+public:
+    FuncTypeNode(const FunctionType ft) : ft_(ft) {}
+    EquelleType type() const
+    {
+        return ft_.returnType();
+    }
+    FunctionType funcType() const
+    {
+        return ft_;
+    }
+private:
+    FunctionType ft_;
+};
+
+typedef FuncTypeNode* FuncTypeNodePtr;
+
+enum BinaryOp { Add, Subtract, Multiply, Divide };
+
+class BinaryOpNode : public Node
+{
+public:
+    BinaryOpNode(BinaryOp op, NodePtr left, NodePtr right) : op_(op), left_(left), right_(right) {}
+private:
+    BinaryOp op_;
+    NodePtr left_;
+    NodePtr right_;
+};
+
+class NormNode : public Node
+{
+public:
+    NormNode(NodePtr expr_to_norm) : expr_to_norm_(expr_to_norm) {}
+private:
+    NodePtr expr_to_norm_;
+};
+
+class UnaryNegationNode : public Node
+{
+public:
+    UnaryNegationNode(NodePtr expr_to_negate) : expr_to_negate_(expr_to_negate) {}
+private:
+    NodePtr expr_to_negate_;
+};
+
+class OnNode : public Node
+{
+public:
+    OnNode(NodePtr left, NodePtr right) : left_(left), right_(right) {}
+private:
+    NodePtr left_;
+    NodePtr right_;
+};
+
+class TrinaryIfNode : public Node
+{
+public:
+    TrinaryIfNode(NodePtr predicate, NodePtr iftrue, NodePtr iffalse)
+        : predicate_(predicate), iftrue_(iftrue), iffalse_(iffalse)
+    {}
+    EquelleType type() const
+    {
+        return iftrue_->type();
+    }
+private:
+    NodePtr predicate_;
+    NodePtr iftrue_;
+    NodePtr iffalse_;
+};
+
+class VarDeclNode : public Node
+{
+public:
+    VarDeclNode(std::string varname, TypeNodePtr type)
+        : varname_(varname), type_(type) {}
+    EquelleType type() const
+    {
+        return type_->type();
+    }
+    const std::string& name() const
+    {
+        return varname_;
+    }
+private:
+    std::string varname_;
+    TypeNodePtr type_;
+};
+
+class VarAssignNode : public Node
+{
+public:
+    VarAssignNode(std::string varname, NodePtr expr) : varname_(varname), expr_(expr) {}
+private:
+    std::string varname_;
+    NodePtr expr_;
+};
+
+class VarNode : public Node
+{
+public:
+    VarNode(const std::string& varname) : varname_(varname) {}
+    EquelleType type() const
+    {
+        return SymbolTable::getCurrentFunction().variableType(varname_);
+    }
+    const std::string& name() const
+    {
+        return varname_;
+    }
+private:
+    std::string varname_;
+};
+
+class FuncArgsDeclNode : public Node
+{
+public:
+    FuncArgsDeclNode(VarDeclNode* vardecl = 0)
+    {
+        if (vardecl) {
+            decls_.push_back(vardecl);
+        }
+    }
+    void addArg(VarDeclNode* vardecl)
+    {
+        decls_.push_back(vardecl);
+    }
+    std::vector<FunctionType::ArgumentType> arguments() const
+    {
+        std::vector<FunctionType::ArgumentType> args;
+        args.reserve(decls_.size());
+        for (auto vdn : decls_) {
+            args.push_back(std::make_pair(vdn->name(), vdn->type()));
+        }
+        return args;
+    }
+private:
+    std::vector<VarDeclNode*> decls_;
+};
+
+class FuncDeclNode : public Node
+{
+public:
+    FuncDeclNode(std::string funcname, FuncTypeNodePtr ftype)
+        : funcname_(funcname), ftype_(ftype) {}
+private:
+    std::string funcname_;
+    FuncTypeNodePtr ftype_;
+};
+
+class FuncAssignNode : public Node
+{
+public:
+    FuncAssignNode(std::string funcname, NodePtr funcargs, NodePtr funcbody)
+        : funcname_(funcname), funcargs_(funcargs), funcbody_(funcbody) {}
+private:
+    std::string funcname_;
+    NodePtr funcargs_;
+    NodePtr funcbody_;
+};
+
+class FuncCallNode : public Node
+{
+public:
+    FuncCallNode(std::string funcname, NodePtr funcargs)
+        : funcname_(funcname), funcargs_(funcargs) {}
+    EquelleType type() const
+    {
+        return SymbolTable::getFunction(funcname_).returnType();
+    }
+private:
+    std::string funcname_;
+    NodePtr funcargs_;
+};
 
 
 // ------ Handle parse events ------
 
 inline NodePtr handleNumber(const double num)
 {
-    return new Number(num);
+    return new NumberNode(num);
 }
 
-inline NodePtr handleDeclaration(const std::string name, NodePtr)
+inline VarDeclNode* handleDeclaration(const std::string name, TypeNodePtr type)
+{
+    SymbolTable::declareVariable(name, type->type());
+    return new VarDeclNode(name, type);
+}
+
+inline NodePtr handleFuncDeclaration(const std::string name, FuncTypeNodePtr ftype)
+{
+    SymbolTable::declareFunction(name, ftype->funcType());
+    return new FuncDeclNode(name, ftype);
+}
+
+inline NodePtr handleDeclarationAssign(const std::string name, TypeNodePtr, NodePtr)
 {
     if (false){//SymbolTable::functionMode()) {
     } else {
         SymbolTable::declareVariable(name, EquelleType());
     }
     return new Node();
+}
+
+inline TypeNodePtr handleCollection(TypeNodePtr btype, NodePtr gridmapping, NodePtr /*subsetof*/)
+{
+    EquelleType bt = btype->type();
+    if (!bt.isBasic()) {
+        std::string errmsg = "attempting to declare a Collection Of <nonsense>";
+        yyerror(errmsg.c_str());
+    }
+    if (gridmapping) {
+        if (!gridmapping->type().isEntityCollection() || gridmapping->type().gridMapping() == NotApplicable) {
+            yyerror("a Collection must be On a Collection of Cell, Face etc.");
+        }
+    }
+    const int gm = gridmapping ? gridmapping->type().gridMapping() : NotApplicable;
+    return new TypeNode(EquelleType(bt.basicType(), true, gm));
+}
+
+inline FuncTypeNodePtr handleFuncType(FuncArgsDeclNode* argtypes, TypeNodePtr rtype)
+{
+    return new FuncTypeNode(FunctionType(rtype->type(), argtypes->arguments()));
 }
 
 
--- atgeirrcompiler/afryacc.y
+++ atgeirrcompiler/afryacc.y
@@ -31,13 +31,15 @@
 %type <node> program
 %type <node> line
 %type <node> statement
-%type <node> declaration
+%type <vardecl> declaration
+%type <node> f_declaration
 %type <node> assignment
 %type <node> comb_decl_assign
 %type <node> expr
-%type <node> type_expr
-%type <node> basic_type
-%type <node> f_decl_args
+%type <type> type_expr
+%type <ftype> f_type_expr
+%type <type> basic_type
+%type <fargdecl> f_decl_args
 %type <node> number
 %type <node> function_call
 %type <node> f_call_args
@@ -71,6 +73,10 @@
 
 %union{
     NodePtr node;
+    TypeNodePtr type;
+    VarDeclNode* vardecl;
+    FuncTypeNodePtr ftype;
+    FuncArgsDeclNode* fargdecl;
     std::string* str;
 }
 
@@ -81,71 +87,75 @@ program: program line           { $$ = new Node(); }
        |                        { $$ = new Node(); }
        ;
 
-line: statement EOL             { $$ = new Node(); }
-    | statement COMMENT EOL     { $$ = new Node(); }
-    | COMMENT EOL               { $$ = new Node(); }
-    | EOL                       { $$ = new Node(); }
+line: statement EOL             { $$ = $1; }
+    | statement COMMENT EOL     { $$ = $1; }
+    | COMMENT EOL               { $$ = 0; }
+    | EOL                       { $$ = 0; }
     ;
 
 fbody: '{' EOL program '}'      { $$ = new Node(); }
 
 statement: declaration          { $$ = new Node(); }
+         | f_declaration        { $$ = new Node(); }
          | assignment           { $$ = new Node(); }
          | comb_decl_assign     { $$ = new Node(); }
-         | expr                 { $$ = new Node(); }
+         | function_call        { $$ = new Node(); }
          | RET expr             { $$ = new Node(); }
 ;
 
 declaration: ID ':' type_expr  { $$ = handleDeclaration(*($1), $3); delete $1; }
 
-assignment: ID '=' expr   { $$ = new Node(); }
-          | ID '(' f_call_args ')' '=' fbody  { $$ = new Node(); }
+f_declaration: ID ':' f_type_expr  { $$ = handleFuncDeclaration(*($1), $3); delete $1; }
+
+assignment: ID '=' expr   { $$ = new VarAssignNode(*($1), $3); delete $1; }
+          | ID '(' f_call_args ')' '=' fbody  { $$ = new FuncAssignNode(*($1), $3, $6); delete $1; }
           ;
 
-comb_decl_assign: ID ':' type_expr '=' expr  { $$ = new Node(); }
+comb_decl_assign: ID ':' type_expr '=' expr  { $$ = handleDeclarationAssign(*($1), $3, $5); delete $1; }
 
-expr: number              { $$ = new Node(); }
-    | function_call       { $$ = new Node(); }
-    | '(' expr ')'        { $$ = new Node(); }
-    | '|' expr '|'        { $$ = new Node(); }
-    | expr '/' expr       { $$ = new Node(); }
-    | expr '*' expr       { $$ = new Node(); }
-    | expr '-' expr       { $$ = new Node(); }
-    | '-' expr %prec UMINUS  { $$ = new Node(); }
-    | expr '+' expr       { $$ = new Node(); }
-    | expr '?' expr ':' expr %prec '?' { $$ = new Node(); }
-    | expr ON expr        { $$ = new Node(); }
-    | ID                  { $$ = new Node(); }
+expr: number              { $$ = $1; }
+    | function_call       { $$ = $1; }
+    | '(' expr ')'        { $$ = $2; }
+    | '|' expr '|'        { $$ = new NormNode($2); }
+    | expr '/' expr       { $$ = new BinaryOpNode(Divide, $1, $3); }
+    | expr '*' expr       { $$ = new BinaryOpNode(Multiply, $1, $3); }
+    | expr '-' expr       { $$ = new BinaryOpNode(Subtract, $1, $3); }
+    | '-' expr %prec UMINUS  { $$ = new UnaryNegationNode($2); }
+    | expr '+' expr       { $$ = new BinaryOpNode(Add, $1, $3); }
+    | expr '?' expr ':' expr %prec '?' { $$ = new TrinaryIfNode($1, $3, $5); }
+    | expr ON expr        { $$ = new OnNode($1, $3); }
+    | ID                  { $$ = new VarNode(*($1)); delete $1; }
     ;
 
-type_expr: basic_type     { $$ = new Node(); }
-         | COLLECTION OF basic_type     { $$ = new Node(); }
-         | COLLECTION OF basic_type ON expr { $$ = new Node(); }
-         | COLLECTION OF basic_type SUBSET OF expr { $$ = new Node(); }
-         | COLLECTION OF basic_type ON expr SUBSET OF expr { $$ = new Node(); }
-         | FUNCTION '(' f_decl_args ')' RET type_expr    { $$ = new Node(); }
+type_expr: basic_type                                      { $$ = $1; }
+         | COLLECTION OF basic_type                        { $$ = handleCollection($3,  0,  0); }
+         | COLLECTION OF basic_type ON expr                { $$ = handleCollection($3, $5,  0); }
+         | COLLECTION OF basic_type SUBSET OF expr         { $$ = handleCollection($3,  0, $6); }
+         | COLLECTION OF basic_type ON expr SUBSET OF expr { $$ = handleCollection($3, $5, $8); }
          ;
 
-basic_type: SCALAR  { $$ = new Node(); }
-          | VECTOR  { $$ = new Node(); }
-          | BOOL    { $$ = new Node(); }
-          | CELL    { $$ = new Node(); }
-          | FACE    { $$ = new Node(); }
-          | EDGE    { $$ = new Node(); }
-          | VERTEX  { $$ = new Node(); }
+f_type_expr: FUNCTION '(' f_decl_args ')' RET type_expr      { $$ = handleFuncType($3, $6); }
+
+basic_type: SCALAR  { $$ = new TypeNode(EquelleType(Scalar)); }
+          | VECTOR  { $$ = new TypeNode(EquelleType(Vector)); }
+          | BOOL    { $$ = new TypeNode(EquelleType(Bool)); }
+          | CELL    { $$ = new TypeNode(EquelleType(Cell)); }
+          | FACE    { $$ = new TypeNode(EquelleType(Face)); }
+          | EDGE    { $$ = new TypeNode(EquelleType(Edge)); }
+          | VERTEX  { $$ = new TypeNode(EquelleType(Vertex)); }
           ;
 
-f_decl_args: f_decl_args ',' declaration { $$ = new Node(); }
-           | declaration                 { $$ = new Node(); }
-           |                             { $$ = new Node(); }
+f_decl_args: f_decl_args ',' declaration { $$ = $1; $$->addArg($3); }
+           | declaration                 { $$ = new FuncArgsDeclNode($1); }
+           |                             { $$ = new FuncArgsDeclNode(); }
            ;
 
 number: INT                     { $$ = handleNumber(numFromString(*($1))); delete $1; }
       | FLOAT                   { $$ = handleNumber(numFromString(*($1))); delete $1; }
       ;
 
-function_call: BUILTIN '(' f_call_args ')'  { $$ = new Node(); }
-             | ID '(' f_call_args ')'       { $$ = new Node(); }
+function_call: BUILTIN '(' f_call_args ')'  { $$ = new FuncCallNode(*($1), $3); delete $1; }
+             | ID '(' f_call_args ')'       { $$ = new FuncCallNode(*($1), $3); delete $1; }
              ;
 
 f_call_args: f_call_args ',' expr     { $$ = new Node(); }
