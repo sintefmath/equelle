3d6268cf1e3fef717f8e567f8193a31b3d9f4d68
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2013-10-28 09:32:48 +0100

    Added generated cpp files to repo.
    To make it easier to track changes to compiled output.

---
+++ examples/app/out_heateq_timesteps.cpp
@@ -0,0 +1,73 @@
+
+// This program was created by the Equelle compiler from SINTEF.
+
+#include <opm/core/utility/parameters/ParameterGroup.hpp>
+#include <opm/core/linalg/LinearSolverFactory.hpp>
+#include <opm/core/utility/ErrorMacros.hpp>
+#include <opm/autodiff/AutoDiffBlock.hpp>
+#include <opm/autodiff/AutoDiffHelpers.hpp>
+#include <opm/core/grid.h>
+#include <opm/core/grid/GridManager.hpp>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <cmath>
+
+#include "EquelleRuntimeCPU.hpp"
+
+int main(int argc, char** argv)
+{
+    // Get user parameters.
+    Opm::parameter::ParameterGroup param(argc, argv, false);
+
+    // Create the Equelle runtime.
+    EquelleRuntimeCPU er(param);
+
+    // ============= Generated code starts here ================
+
+    const Scalar k = er.userSpecifiedScalarWithDefault("k", double(0.3));
+    const CollOfScalar u_initial = er.userSpecifiedCollectionOfScalar("u_initial", er.allCells());
+    const CollOfFace dirichlet_boundary = er.userSpecifiedCollectionOfFaceSubsetOf("dirichlet_boundary", er.boundaryFaces());
+    const CollOfScalar dirichlet_val = er.userSpecifiedCollectionOfScalar("dirichlet_val", dirichlet_boundary);
+    const CollOfScalar vol = er.norm(er.allCells());
+    const CollOfFace interior_faces = er.interiorFaces();
+    const CollOfCell first = er.firstCell(interior_faces);
+    const CollOfCell second = er.secondCell(interior_faces);
+    const CollOfScalar itrans = (k * (er.norm(interior_faces) / er.norm((er.centroid(first) - er.centroid(second)))));
+    const CollOfFace bf = er.boundaryFaces();
+    const CollOfCell bf_cells = er.trinaryIf(er.isEmpty(er.firstCell(bf)), er.secondCell(bf), er.firstCell(bf));
+    const CollOfScalar bf_sign = er.trinaryIf(er.isEmpty(er.firstCell(bf)), er.operatorOn(-double(1), bf), er.operatorOn(double(1), bf));
+    const CollOfScalar btrans = (k * (er.norm(bf) / er.norm((er.centroid(bf) - er.centroid(bf_cells)))));
+    const CollOfScalar dir_sign = er.operatorOn(bf_sign, er.boundaryFaces(), dirichlet_boundary);
+    auto computeInteriorFlux = [&](const CollOfScalar& u) -> CollOfScalar {
+        return (-itrans * er.gradient(u));
+    };
+    auto computeBoundaryFlux = [&](const CollOfScalar& u) -> CollOfScalar {
+        const CollOfScalar u_dirbdycells = er.operatorOn(u, er.allCells(), er.operatorOn(bf_cells, er.boundaryFaces(), dirichlet_boundary));
+        const CollOfScalar dir_fluxes = ((er.operatorOn(btrans, er.boundaryFaces(), dirichlet_boundary) * dir_sign) * (u_dirbdycells - dirichlet_val));
+        return er.operatorOn(dir_fluxes, dirichlet_boundary, er.boundaryFaces());
+    };
+    auto computeResidual = [&](const CollOfScalar& u, const CollOfScalar& u0, const Scalar& dt) -> CollOfScalar {
+        const CollOfScalar ifluxes = computeInteriorFlux(u);
+        const CollOfScalar bfluxes = computeBoundaryFlux(u);
+        const CollOfScalar fluxes = (er.operatorOn(ifluxes, er.interiorFaces(), er.allFaces()) + er.operatorOn(bfluxes, er.boundaryFaces(), er.allFaces()));
+        const CollOfScalar residual = ((u - u0) + ((dt / vol) * er.divergence(fluxes)));
+        return residual;
+    };
+    const SeqOfScalar timesteps = er.userSpecifiedSequenceOfScalar("timesteps");
+    CollOfScalar u0;
+    u0 = u_initial;
+    for (const Scalar& dt : timesteps) {
+        auto computeResidualLocal = [&](const CollOfScalar& u) -> CollOfScalar {
+            return computeResidual(u, u0, dt);
+        };
+        const CollOfScalar u_guess = u0;
+        const CollOfScalar u = er.newtonSolve(computeResidualLocal, u_guess);
+        er.output("u", u);
+        u0 = u;
+    }
+
+    // ============= Generated code ends here ================
+
+    return 0;
+}
---
+++ examples/app/out_scopetest.cpp
@@ -0,0 +1,52 @@
+
+// This program was created by the Equelle compiler from SINTEF.
+
+#include <opm/core/utility/parameters/ParameterGroup.hpp>
+#include <opm/core/linalg/LinearSolverFactory.hpp>
+#include <opm/core/utility/ErrorMacros.hpp>
+#include <opm/autodiff/AutoDiffBlock.hpp>
+#include <opm/autodiff/AutoDiffHelpers.hpp>
+#include <opm/core/grid.h>
+#include <opm/core/grid/GridManager.hpp>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <cmath>
+
+#include "EquelleRuntimeCPU.hpp"
+
+int main(int argc, char** argv)
+{
+    // Get user parameters.
+    Opm::parameter::ParameterGroup param(argc, argv, false);
+
+    // Create the Equelle runtime.
+    EquelleRuntimeCPU er(param);
+
+    // ============= Generated code starts here ================
+
+    const Scalar a = double(5);
+    const Scalar b = double(6);
+    auto foo = [&](const Scalar& x, const Scalar& y) -> Scalar {
+        return ((a * x) + (b * y));
+    };
+    const SeqOfScalar seq = er.userSpecifiedSequenceOfScalar("seq");
+    for (const Scalar& elem : seq) {
+        const Scalar r = ((a + double(3)) + elem);
+        const SeqOfScalar seq2 = er.userSpecifiedSequenceOfScalar("seq2");
+        for (const Scalar& e2 : seq2) {
+            auto foo3 = [&](const Scalar& y) -> Scalar {
+                auto three = [&]() -> Scalar {
+                    return double(3);
+                };
+                return foo(three(), y);
+            };
+            const Scalar q = ((b + foo3(e2)) + r);
+            er.output("q", q);
+        }
+    }
+
+    // ============= Generated code ends here ================
+
+    return 0;
+}
