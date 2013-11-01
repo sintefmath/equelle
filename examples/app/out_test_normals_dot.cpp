e588862de0b420076b7bca1f9b323d069e6ef150
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2013-11-01 18:23:58 +0100

    Updated example to test vector random access.

--- examples/app/out_test_normals_dot.cpp
+++ examples/app/out_test_normals_dot.cpp
@@ -15,6 +15,8 @@
 
 #include "EquelleRuntimeCPU.hpp"
 
+void ensureRequirements(const EquelleRuntimeCPU& er);
+
 int main(int argc, char** argv)
 {
     // Get user parameters.
@@ -23,13 +25,22 @@ int main(int argc, char** argv)
     // Create the Equelle runtime.
     EquelleRuntimeCPU er(param);
 
+    ensureRequirements(er);
+
     // ============= Generated code starts here ================
 
     const CollOfVector n = er.normal(er.allFaces());
     const CollOfScalar n2 = er.dot(n, n);
+    const CollOfScalar n0 = CollOfScalar(n.col(0));
     er.output("squared normals", n2);
+    er.output("first component", n0);
 
     // ============= Generated code ends here ================
 
     return 0;
+}
+
+void ensureRequirements(const EquelleRuntimeCPU& er)
+{
+    er.ensureGridDimensionMin(1);
 }
