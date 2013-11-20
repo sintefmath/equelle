b43528f1dc558cacdfbc2fc7bdd1bc2b5674bcb4
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2013-11-20 09:47:54 +0100

    New example demonstrating variable binding.

---
+++ examples/app/out_context.cpp
@@ -0,0 +1,49 @@
+
+// This program was created by the Equelle compiler from SINTEF.
+
+#include <opm/core/utility/parameters/ParameterGroup.hpp>
+#include <opm/core/linalg/LinearSolverFactory.hpp>
+#include <opm/core/utility/ErrorMacros.hpp>
+#include <opm/autodiff/AutoDiffBlock.hpp>
+#include <opm/autodiff/AutoDiffHelpers.hpp>
+#include <opm/core/grid.h>
+#include <opm/core/grid/GridManager.hpp>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <cmath>
+#include <array>
+
+#include "EquelleRuntimeCPU.hpp"
+
+void ensureRequirements(const EquelleRuntimeCPU& er);
+
+int main(int argc, char** argv)
+{
+    // Get user parameters.
+    Opm::parameter::ParameterGroup param(argc, argv, false);
+
+    // Create the Equelle runtime.
+    EquelleRuntimeCPU er(param);
+
+    ensureRequirements(er);
+
+    // ============= Generated code starts here ================
+
+    Scalar a;
+    a = double(8);
+    auto f = [&]() -> Scalar {
+        return (double(2) * a);
+    };
+    er.output("f before", f());
+    a = double(3);
+    er.output("f after", f());
+
+    // ============= Generated code ends here ================
+
+    return 0;
+}
+
+void ensureRequirements(const EquelleRuntimeCPU& er)
+{
+}
