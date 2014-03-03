2e1572792d0bc4123d22afb5c04830c992e282d5
Johan Seland <johan.seland@johanseland.com>
2014-03-03 13:49:47 +0100

    Added basic scaffolding for zoltanIntegration.

---
+++ backends/MPI/test/src/zoltanIntegration.cpp
@@ -0,0 +1,10 @@
+#define BOSOT_TEST_MAIN
+#define BOOST_TEST_MODULE EquelleControllerTest
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE( testFactoryMethod )
+{
+    BOOST_ASSERT( true);
+}
+
