29f2a683c9140e5fc576ea62a38f22b1a9bd9b73
Johan Seland <johan.seland@johanseland.com>
2014-03-03 21:31:40 +0100

    Added SAFE_CALL macros for Zoltan and MPI.

---
+++ backends/MPI/include/equelle/mpiutils.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <stdexcept>
+#include <sstream>
+
+#define MPI_SAFE_CALL( err ) equelle::mpi::mpiSafeCall( err, __FILE__, __LINE__, __FUNCITON__ )
+#define ZOLTAN_SAFE_CALL( err ) equelle::mpi::zoltanSafeCall( err, __FILE__, __LINE__, __FUNCITON__ )
+
+namespace equelle { namespace mpi {
+
+static const
+
+inline
+void zoltanSafeCall( const int err, const std::string file, const int line, const std::string functionName ) {
+    if ( err != ZOLTAN_OK ) {
+        std::stringstream ss;
+
+        ss << "Zoltan returned: ";
+        switch( err ) {
+        case ZOLTAN_WARN:
+            ss << "ZOLTAN_WARN";
+            break;
+        case ZOLTAN_FATAL:
+            ss << "ZOLTAN_FATAL";
+            break;
+        case ZOLTAN_MEMERR:
+            ss << "ZOLTAN_MEMERR";
+            break;
+        default:
+            ss << "Unknown Zoltan error code";
+        }
+
+        ss << "File: " << file << std::endl;
+        ss << "Line: " << line << std::endl;
+        ss << "Function" << functionName << std::endl;
+        throw std::runtime_error( ss.str() );
+    }
+}
+
+inline
+void mpiSafeCall( const int err, const std::string file, const int line, const std::string functionName ) {
+    if ( err == MPI_SUCCESS ) {
+        return;
+    }
+
+    int errlen = 0;
+    char message[MPI_MAX_ERROR_STRING]{};
+    MPI_Error_string( err, message, &errlen );
+
+    std::stringstream ss;
+    ss << "MPI error code: " << err << " " << message;
+    ss << "at File: " << file << std::endl;
+    ss << "Line: " << line << std::endl;
+    ss << "Function: " << functionName << std::endl;
+
+    throw std::runtime_error( ss.str() );
+}
+
+
+
+
+}} // namespace equelle::mpi
+
--- backends/MPI/test/src/zoltanIntegration.cpp
+++ backends/MPI/test/src/zoltanIntegration.cpp
@@ -1,12 +1,23 @@
 #define BOSOT_TEST_MAIN
 #define BOOST_TEST_MODULE EquelleControllerTest
+
+#include <memory>
 #include <boost/test/unit_test.hpp>
 
-#include <zoltan.h>
 
+#include <zoltan_cpp.h>
+#include "EquelleRuntimeCPU.hpp"
+#include "equelle/mpiutils.hpp"
 
-BOOST_AUTO_TEST_CASE( testFactoryMethod )
+BOOST_AUTO_TEST_CASE( giveGridToZoltan )
 {
-    BOOST_ASSERT( true);
+    Opm::parameter::ParameterGroup paramgroup;
+
+    auto grid = equelle::createGridManager(paramgroup);
+
+    BOOST_CHECK_EQUAL( grid->c_grid()->number_of_cells, 6 );
+
+
+
 }
 
