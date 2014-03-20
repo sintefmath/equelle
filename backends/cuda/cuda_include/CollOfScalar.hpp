48f9dca964bc6bbfabc8a61f1816fa665bc5f992
havahol <havardhholm@gmail.com>
2014-03-20 11:18:29 +0100

    Implemented CollOfScalar > Scalar and CollOfScalar < CollOfScalar

--- backends/cuda/cuda_include/CollOfScalar.hpp
+++ backends/cuda/cuda_include/CollOfScalar.hpp
@@ -228,10 +228,38 @@ namespace equelleCUDA {
       \param[in] rhs Right hand side values
       \param[in] size Size of the arrays.
     */
-    __global__ void compGTkernel( bool* out,
-				  const double* lhs,
-				  const double* rhs,
-				  const int size);
+    __global__ void comp_collGTcoll_kernel( bool* out,
+					    const double* lhs,
+					    const double* rhs,
+					    const int size);
+
+    //! CUDA kernel for greater than scalar operation
+    /*!
+      Compare elements in lhs with a single scalar rhs and return a Collection of Booleans.
+      \code out[i] = lhs[i] > rhs \endcode
+      \param[out] The resulting collection of booleans
+      \param[in] lhs Left hand side collection of scalars
+      \param[in] rhs Right hand side scalar
+      \param[in] size Size of the lhs array.
+     */
+    __global__ void comp_collGTscal_kernel( bool* out,
+					    const double* lhs,
+					    const double rhs,
+					    const int size);
+
+    //! CUDA kernel for less than operation
+    /*!
+      Compare elements in lhs with elements in rhs and return a Collection Of Booleans.
+      \code out[i] = lhs[i] < rhs[i] \endcode
+      \param[out] The resulting collection of booleans
+      \param[in] lhs Left hand side values
+      \param[in] rhs Right hand side values
+      \param[in] size Size of the arrays
+    */
+    __global__ void comp_collLTcoll_kernel( bool* out,
+					    const double* lhs,
+					    const double* rhs,
+					    const int size);
 
     // -------------- Operation overloading ------------------- //
     
@@ -323,10 +351,22 @@ namespace equelleCUDA {
       Greater than operator
       \return Collection of Booleans consisting of
       \code out[i] = lhs[i] > rhs[i] \endcode
-     */
+    */
     CollOfBool operator>(const CollOfScalar& lhs, const CollOfScalar& rhs);
 
+    /*!
+      Greater than operator comparing with a scalar
+      \return Collection of Booleans consisting of
+      \code out[i] = lhs[i] > rhs \endcode
+    */
+    CollOfBool operator>(const CollOfScalar& lhs, const Scalar rhs);
 
+    /*! 
+      Less than operator
+      \return Collection of Booleans consisting of
+      \coded out[i] = lhs[i] < rhs[i] \endcode
+    */
+    CollOfBool operator<(const CollOfScalar& lhs, const CollOfScalar& rhs);
 
     //! CollOfBool -> std::vector<bool>
     /*!
