a8e35ad914d7f6ec20c418a7f7c0b620346bc3e7
Johan Seland <johan.seland@johanseland.com>
2014-03-24 11:07:29 +0100

    Added scaffolding for experimental/cartesian_test
    - Build system updates
    - Basic .cpp file.

---
+++ experimental/cartesian/src/cartesian_test.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE EquelleCartesianTest
+
+#include <memory>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <numeric>
+#include <fstream>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( hello ) {
+    BOOST_MESSAGE( "I am alive" );
+}
