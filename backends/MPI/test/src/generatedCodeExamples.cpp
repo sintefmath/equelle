8d8286c6a9a649b3d91cbd1867f9cf7686ebc46c
Johan Seland <johan.seland@johanseland.com>
2014-04-02 13:00:44 +0200

    Expanded and cleaned up mpi-test basicCalculator.

--- backends/MPI/test/src/generatedCodeExamples.cpp
+++ backends/MPI/test/src/generatedCodeExamples.cpp
@@ -13,8 +13,40 @@
 
 using namespace equelle;
 
+namespace {
+
+// These make testing a lot easier.
+
+std::vector<double> operator+( std::vector<double>& v0, std::vector<double>& v1 ) {
+    std::vector<double> res( v0.size() );
+    std::transform( v0.begin(), v0.end(), v1.begin(), res.begin(),
+                    []( double a, double b ) { return a + b; } );
+    return res;
+}
+
+std::vector<double> operator-( std::vector<double>& v0, std::vector<double>& v1 ) {
+    std::vector<double> res( v0.size() );
+    std::transform( v0.begin(), v0.end(), v1.begin(), res.begin(),
+                    []( double a, double b ) { return a - b; } );
+    return res;
+}
+
+std::vector<double> operator*( std::vector<double>& v0, std::vector<double>& v1 ) {
+    std::vector<double> res( v0.size() );
+    std::transform( v0.begin(), v0.end(), v1.begin(), res.begin(),
+                    []( double a, double b ) { return a * b; } );
+    return res;
+}
+
+std::vector<double> operator/( std::vector<double>& v0, std::vector<double>& v1 ) {
+    std::vector<double> res( v0.size() );
+    std::transform( v0.begin(), v0.end(), v1.begin(), res.begin(),
+                    []( double a, double b ) { return a / b; } );
+    return res;
+}
 
 
+}
 BOOST_AUTO_TEST_CASE( basicCalculator ) {
     // Get user parameters.
 
@@ -23,65 +55,56 @@ BOOST_AUTO_TEST_CASE( basicCalculator ) {
     param.disableOutput();
 
     // These are global input data.
-    std::vector<double> a_0 = { 0, 1, 2, 3, 4, 5 };
-    std::vector<double> b_0 = {5, 4, 3, 2, 1, 0 };
-    std::vector<double> d_0 = {2, 2, 2, 2, 2, 2 };
+    std::vector<double> a_gold = {0, 1, 2, 3, 4, 5 };
+    std::vector<double> b_gold = {5, 4, 3, 2, 1, 0 };
+    std::vector<double> d_gold = {2, 2, 2, 2, 2, 2 };
 
-    injectMockData( param, "a", a_0.begin(), a_0.end() );
-    injectMockData( param, "b", b_0.begin(), b_0.end() );
-    injectMockData( param, "d", d_0.begin(), d_0.end() );
+    injectMockData( param, "a", a_gold.begin(), a_gold.end() );
+    injectMockData( param, "b", b_gold.begin(), b_gold.end() );
+    injectMockData( param, "d", d_gold.begin(), d_gold.end() );
 
     // Create the Equelle runtime.
     equelle::RuntimeMPI er( param );
     er.decompose();
     //ensureRequirements(er);
 
-
-
+    // Code from examples/app/smallCals.cpp
     // ============= Generated code starts here ================
-    BOOST_MESSAGE( er.subGrid.global_cell.size() );
     const CollOfScalar a = er.inputCollectionOfScalar("a", er.allCells());
     const CollOfScalar b = er.inputCollectionOfScalar("b", er.allCells());
-
-    const CollOfScalar c = (a - b);
-    std::vector<double> c_global = {-5, -3, -1, 1, 3, 5 };
-
-    for( auto i = 0; i < er.subGrid.global_cell.size(); ++i ) {
-        auto gid = er.subGrid.global_cell[i];
-        BOOST_CHECK_EQUAL( c.value()[i], c_global[gid] );
-    }
-
     const CollOfScalar d = er.inputCollectionOfScalar("d", er.allCells());
-
-    const CollOfScalar e = (d - c);
-
-    std::vector<double> e_global = { 2+5, 2+3, 2+1, 2-1, 2-3, 2-5 };
-    for( auto i = 0; i < er.subGrid.global_cell.size(); ++i ) {
-        auto gid = er.subGrid.global_cell[i];
-        BOOST_CHECK_EQUAL( e.value()[i], e_global[gid] );
-    }
-
-    const CollOfScalar f = (e / d);
-
-    std::vector<double> f_global;
-    std::transform( e_global.begin(), e_global.end(), d_0.begin(), std::back_inserter( f_global ),
-                    []( double e, double d ) { return e / d; } );
-
-    for( auto i = 0; i < er.subGrid.global_cell.size(); ++i ) {
-        auto gid = er.subGrid.global_cell[i];
-        BOOST_CHECK_EQUAL( f.value()[i], f_global[gid] );
-    }
-
-/*    const CollOfScalar d = er.inputCollectionOfScalar("d", er.allCells());
     const CollOfScalar c = (a - b);
     const CollOfScalar e = (d - c);
-    er.output("e", e);
     const CollOfScalar f = (e / d);
-    er.output("f", f);
     const CollOfScalar g = (f + b);
-    er.output("g", g);
     const CollOfScalar h = (g * d);
-    er.output("h", h);
-*/
+
     // ============= Generated code ends here ================
+
+    const auto c_global = er.allGather( c );
+    const auto e_global = er.allGather( e );
+    const auto f_global = er.allGather( f );
+    const auto g_global = er.allGather( g );
+    const auto h_global = er.allGather( h );
+
+    auto c_gold = (a_gold - b_gold);
+    auto e_gold = (d_gold - c_gold);
+    auto f_gold = (e_gold / d_gold);
+    auto g_gold = (f_gold + b_gold);
+    auto h_gold = (g_gold * d_gold);
+
+    BOOST_CHECK_EQUAL_COLLECTIONS( c_global.value().data(), c_global.value().data() +  6,
+                                   c_gold.begin(), c_gold.end() );
+
+    BOOST_CHECK_EQUAL_COLLECTIONS( e_global.value().data(), e_global.value().data() +  6,
+                                   e_gold.begin(), e_gold.end() );
+
+    BOOST_CHECK_EQUAL_COLLECTIONS( f_global.value().data(), f_global.value().data() +  6,
+                                   f_gold.begin(), f_gold.end() );
+
+    BOOST_CHECK_EQUAL_COLLECTIONS( g_global.value().data(), g_global.value().data() +  6,
+                                   g_gold.begin(), g_gold.end() );
+
+    BOOST_CHECK_EQUAL_COLLECTIONS( h_global.value().data(), h_global.value().data() +  6,
+                                   h_gold.begin(), h_gold.end() );
 }
