8de0cbb7e932f05fca6d32abd648b4a88999d2b5
Johan Seland <johan.seland@johanseland.com>
2014-04-07 15:11:36 +0200

    Updated EquelleRuntimeCPU(grid, param) ctor to properly initialize all members.
    - This clears up the strange bug with output_to_file initialized to
    random values.
    - We could use delegating ctors here, that mandates the use of a
    temporary grid-manager. The current implemenation returns an unmanaged
    GridManager-pointer.

--- backends/serial/src/EquelleRuntimeCPU.cpp
+++ backends/serial/src/EquelleRuntimeCPU.cpp
@@ -67,7 +67,12 @@ EquelleRuntimeCPU::EquelleRuntimeCPU(const Opm::parameter::ParameterGroup& param
 EquelleRuntimeCPU::EquelleRuntimeCPU(const UnstructuredGrid *grid, const Opm::parameter::ParameterGroup &param)
     : grid_( *grid ),
       ops_(grid_),
-      param_( param )
+      linsolver_(param),
+      output_to_file_(param.getDefault("output_to_file", false)),
+      verbose_(param.getDefault("verbose", 0)),
+      param_(param),
+      max_iter_(param.getDefault("max_iter", 10)),
+      abs_res_tol_(param.getDefault("abs_res_tol", 1e-6))
 {
 
 }
