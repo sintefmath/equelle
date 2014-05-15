a68724272f1fc9276fd2e11ca3108f40209682c5
havahol <havardhholm@gmail.com>
2014-05-15 14:34:17 +0200

    compile examples did not follow new input file style

--- examples/compile_examples.sh
+++ examples/compile_examples.sh
@@ -4,5 +4,5 @@ for i in $(ls dsl/*.equelle)
 do
     echo "Compiling $i"
     bn=$(basename $i .equelle)
-    ../../equelle-build/compiler/ec < $i > "app/out_$bn.cpp"
+    ../../equelle-build/compiler/ec -i $i --backend=cpu > "app/out_$bn.cpp"
 done
