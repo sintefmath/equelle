9b61890097dac8e55b8da1722771be1656df02b9
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2014-10-06 11:17:51 +0200

    Add initial version of Dimension class.

---
+++ compiler/Dimension.hpp
@@ -0,0 +1,70 @@
+/*
+  Copyright 2014 SINTEF ICT, Applied Mathematics.
+*/
+
+#ifndef DIMENSION_HEADER_INCLUDED
+#define DIMENSION_HEADER_INCLUDED
+
+#include <array>
+
+enum BaseDimension {
+    Length,
+    Time,
+    Mass,
+    Temperature,
+    ElectricCurrent,
+    QuantityOfSubstance,
+    LuminousIntensity
+};
+
+class Dimension
+{
+public:
+    // Accept implicit conversion from array.
+    Dimension(const std::array<int,7>& dim)
+        : dim_(dim)
+    {
+    }
+
+    Dimension operator+ (const Dimension& other)
+    {
+        Dimension result(*this);
+        for (int dd = 0; dd < 7; ++dd) {
+            result.dim_[dd] += other.dim_[dd];
+        }
+        return result;
+    }
+
+    Dimension operator- (const Dimension& other)
+    {
+        Dimension result(*this);
+        for (int dd = 0; dd < 7; ++dd) {
+            result.dim_[dd] -= other.dim_[dd];
+        }
+        return result;
+    }
+
+    Dimension operator* (const int power)
+    {
+        Dimension result(*this);
+        for (int dd = 0; dd < 7; ++dd) {
+            result.dim_[dd] *= power;
+        }
+        return result;
+    }
+
+    bool operator== (const Dimension& other)
+    {
+        return dim_ == other.dim_;
+    }
+
+    bool operator!= (const Dimension& other)
+    {
+        return !(*this == other);
+    }
+
+private:
+    std::array<int,7> dim_;
+};
+
+#endif // DIMENSION_HEADER_INCLUDED
