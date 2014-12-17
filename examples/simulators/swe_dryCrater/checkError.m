6d3e3c8527b00b8b3c9610a29180e93e0d8b7b5e
havahol <havardhholm@gmail.com>
2014-12-17 14:38:53 +0100

    Clean-up in swe_dryCreater

--- examples/simulators/swe_dryCrater/checkError.m
+++
@@ -1,9 +0,0 @@
-% Compare result from 9th timestep in serial and cuda version
-
-cuda9 = load('q1-00100.output');
-serial9 = load('serial_q1-00100.output');
-
-maxDiff = max(abs(cuda9 - serial9))
-norm = sqrt(sum(cuda9.*cuda9 - serial9.*serial9))
-
-maxVal = max(cuda9)
